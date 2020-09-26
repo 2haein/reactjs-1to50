24ebb3a6ee96358e99a6486c254245c07f170b13
2haein <eehaein2@gmail.com>
2020-09-26 18:27:15 +0900

    Updates

---
+++ precache-manifest.5b3116eb14bc30f32f84928ca17bd02e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dffe21fa6cade654367cfc12998d1dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7ddc3bb6ded0b4a93cf",
+    "url": "/static/js/2.e590e356.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e590e356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c4ba7e94d28ef0c988",
+    "url": "/static/js/main.fcb00cee.chunk.js"
+  },
+  {
+    "revision": "30ea6b3ffc7d7d3563d4",
+    "url": "/static/js/runtime-main.dfa31d90.js"
+  }
+]);
